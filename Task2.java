3a33a046ec26bb8e7503cb385824dae50a72199e
dev-varshan <manojvarshan374@gmail.com>
2026-01-12 17:26:48 +0530

    Task 2

---
+++ Task2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Task2 {
+    public static void main(String[] args) {
+       int [] numbers={1,2,3,4,5};
+
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a Index value: ");
+       int i = sc.nextInt();
+
+       System.out.println("Value at index " + i + " is: " + numbers[i]);
+
+
+    }}
